18e10681bf5d961c9a983460f8c42f20299a7eac
ecclesias <77536370+zeviel@users.noreply.github.com>
2023-03-05 01:56:55 +0800

    Update conferences.sh

--- src/conferences.sh
+++ src/conferences.sh
@@ -88,5 +88,4 @@ function add_conference() {
 		--user-agent "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36" \
 		--header "content-type: application/json" \
 		--header "xvk: $params"
-
 }
